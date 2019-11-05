273d5b8fccad07507fb1e68e69104dc265dba4f3
Mark Artishuk <markyshuk@gmail.com>
2019-11-05 11:42:57 -0800

    Initial project setup and made Todo component

---
+++ src/components/Todo/Todo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Todo = ({ title }) => {
+  return (
+    <div className="todo">
+      <h2>{title}</h2>
+    </div>
+  );
+};
+
+export default Todo;
---
+++ src/reducers/todoReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  item: "Learn about reducers",
+  completed: false,
+  id: 3892987589
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
